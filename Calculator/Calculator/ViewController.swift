081c8506047ecf2a4cf02f1c4c899bf0793278a3
Cindy Liu <cindyliu@berkeley.edu>
2015-12-27 19:07:48 -0800

    calculadora

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Cindy Liu on 12/27/15.
+//  Copyright © 2015 Cindy Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+   
+
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+        print("digit = \(digit)")
+        
+        
+    }
+
+}
+
